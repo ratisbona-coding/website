bb4def2a461c1c4696f5e9aa0743181b4daceb6d
Konstantin Seitz <konstantin.seitz@tchibo.de>
2022-11-11 14:50:17 +0100

    feat: add tailwind boilerplate

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
+<script>
+    import Nav from './../lib/Nav.svelte';
+    import "../app.css";
+</script>
+
+<Nav/>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+  
