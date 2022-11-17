91c236132929b680ff3f0c88cad77aa76d941b5c
Markus Guder <markus.guder@gmail.com>
2022-11-17 20:08:53 +0100

    style: 💄 add baseComponet as basic wrapper

---
+++ src/routes/landing/BaseComponent.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	export let headline: string;
+	export let id: string;
+</script>
+
+<section class="container mx-auto px-6 p-10" {id}>
+	<h2 class="text-4xl font-bold text-center text-black-800 mb-8">{headline}</h2>
+	<hr class="my-4 mx-auto w-20 h-1 bg-gray-800 rounded border-0 md:my-10 dark:bg-gray-700" />
+	<slot />
+</section>
