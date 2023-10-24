10acb66f4668f2948a779cd5a466f3a9cc29b815
marc101101 <markus.guder@gmail.com>
2023-10-24 11:30:41 +0200

    fix header

--- src/routes/landing/Hero.svelte
+++ src/routes/landing/Hero.svelte
@@ -1,12 +1,12 @@
 <script>
-	import hero from '$lib/images/header.svg';
+	import hero from '$lib/images/header.png';
 </script>
 
 <section class="bg-white dark:bg-gray-900 mt-8">
 	<div class="grid px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
-		<div class="mr-auto place-self-center lg:col-span-5">
+		<div class="mr-auto place-self-center lg:col-span-4">
 			<h1
-				class="max-w-2xl mb-4 text-3xl font-extrabold tracking-tight leading-none md:text-4xl xl:text-5xl dark:text-white mt-8"
+				class="max-w-2xl mb-4 text-3xl font-extrabold tracking-tight leading-none md:text-4xl xl:text-6xl dark:text-white mt-8"
 			>
 				Deine Coding-Events in Regensburg
 			</h1>
@@ -16,8 +16,8 @@
 				Wir organisieren das ganze Jahr über eine Vielzahl an Tech-Events.
 			</p>
 		</div>
-		<div class="lg:mt-0 lg:col-span-7 lg:flex mx-auto p-8">
-			<img src={hero} alt="mockup" style="width: 40rem;" />
+		<div class="lg:mt-0 lg:col-span-8 lg:flex mx-auto p-8">
+			<img src={hero} alt="mockup" />
 		</div>
 	</div>
 </section>
