cd87cb24bb6abb5a7b97a9972c844405ee88022e
marc101101 <markus.guder@gmail.com>
2023-11-08 20:04:21 +0100

    added darkmode

--- src/routes/store.ts
+++ src/routes/store.ts
@@ -1,6 +1,15 @@
-import { writable } from 'svelte/store';
+import { writable, type PersistentStore } from "@macfja/svelte-persistent-store"
 
 //read darkmode from lokalstorage
 //export const darkMode = writable(localStorage.getItem('darkMode'));
-export const darkMode = writable(false);
+export const darkMode = writable("darkMode") as PersistentStore<boolean>;
 
+import { browser } from '$app/environment';
+
+if (browser) {
+    darkMode.subscribe((value) => {
+        value
+            ? document.documentElement.classList.add('dark')
+            : document.documentElement.classList.remove('dark');
+    });
+}
